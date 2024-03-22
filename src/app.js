7fc150a4fc78fe7fed51c946364aab3849ce51be
Loisinho <159461422+loisinho87@users.noreply.github.com>
2024-03-22 11:53:23 +0000

    “mensaje”

--- src/app.js
+++ src/app.js
@@ -6,6 +6,43 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("La página se ha cargado correctamente.");
+  let Quien = [
+    "O can",
+    "Miña avoa",
+    "A miña tortuga",
+    "O meu sapoconcho",
+    "O meu neto"
+  ];
+  let Accion = ["comeuse", "aplastouse", "roubou", "rompeu", "ensuciou"];
+  let Que = [
+    "a miña tarefa",
+    "o meu almuerzo",
+    "o meu mobil",
+    "o meu portatil",
+    "a cociña"
+  ];
+  let Cuando = [
+    "antes das clases",
+    "xusto cando terminei",
+    "mentras almorzaba",
+    "mentras durmía"
+  ];
+
+  function generarExcusita() {
+    let randomQuien = Quien[Math.floor(Math.random() * Quien.length)];
+    let randomAccion = Accion[Math.floor(Math.random() * Accion.length)];
+    let randomQue = Que[Math.floor(Math.random() * Que.length)];
+    let randomCuando = Cuando[Math.floor(Math.random() * Cuando.length)];
+    return `${randomQuien} ${randomAccion} ${randomQue} ${randomCuando}`;
+  }
+  console.log(generarExcusita());
+
+  let excusita = document.querySelector("#excusita");
+
+  excusita.addEventListener("click", function() {
+    console.log("Se hizo clic en el párrafo.");
+    let novaExcusa = generarExcusita();
+    excusita.innerText = novaExcusa;
+  });
 };
